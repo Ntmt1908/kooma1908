cd466b2aceddfd32c7cce65bd2031abcd296fec0
Ntmt1908 <151108800+Ntmt1908@users.noreply.github.com>
2023-11-21 23:45:06 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+	autoplayTimeout:5000,
+	smartSpeed:2000,
+	responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
